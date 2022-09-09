b00924a1ae6c939018b9f9e7c361f8f4fd0b97f3
mayaSamb <mayasamb43@gmail.com>
2022-09-08 19:10:45 -0700

    t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>                                                                                                                              
                                                                                                                                                 
 /**                                                                                                                                             
- *  *  main - print a text using puts()                                                                                                            
- *   *                                                                                                                                              
- *    * Description : the text \"Programming is like building a multilingual puzzle                                                                  
- *     *                                                                                                                                              
- *      * Return: return always 0                                                                                                                      
- *       */                                                                                                                                             
+ *  main - print a text using puts()                                                                                                            
+ *                                                                                                                                               * Description : the text \"Programming is like building a multilingual puzzle                                                                  
+ *          
+ * Return: return always 0                                                                                                                      
+ */                                                                                                                                             
                                                                                                                                                 
 int main(void)                                                                                                                                  
 {                                                                                                                                               
 	                                                                                                                                                
-	        puts ("\"Programming is like building a multilingual puzzle") :                                                                         
+	        puts ("\"Programming is like building a multilingual puzzle") ;                                                                         
 			        return (0);                                                                                                                             
 }                 
