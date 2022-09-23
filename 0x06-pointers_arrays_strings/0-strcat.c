ff3eae1ed810d11b20e9c1b72ff9a07e96a786bc
mayaSamb <mayasamb43@gmail.com>
2022-09-22 18:08:13 -0700

    t

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
- *          null byte, to the end of the string pointed to by @dest.
+ * _strcat - Concatenates two strings.
+ *
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
@@ -11,13 +11,14 @@
  */
 char *_strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	char *copy = dest;
+		int index = 1;
 
-	while (dest[index++])
-		dest_len++;
+	while (*++copy)
+		index++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*src)
+		*(dest + index++) = *src++;
 
 	return (dest);
 }
