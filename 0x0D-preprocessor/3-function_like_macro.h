b08de98fe93eac7c13080ffbfbce78bf57ea2552
maya Samb <mayasmb43@gmail.com>
2022-10-10 16:37:27 -0700

    t

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-#define ABS(x) (x >= 0 ? (x) : (-x))
-#endif&
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) > (0) ? (x) : (x) * (-1))
+
+#endif
