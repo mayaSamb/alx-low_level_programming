66a5ffce894af7d2445b50a87a533fa47f5a2d0f
maya Samb <mayasmb43@gmail.com>
2022-10-12 18:24:39 -0700

    t

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/*
- * File: function_pointers.h
- * AUth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: the function to send the name to
+ * Return: nothing
  */
-
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
